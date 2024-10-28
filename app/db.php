140487628a98199a43b4a1ce7f4da65e68a1c574
mosama88 <mosama88@hotmail.com>
2024-10-28 14:28:19 +0300

    OO

---
+++ app/db.php
@@ -0,0 +1,34 @@
+<?php
+
+define("hostname","localhost");
+define("username","root");
+define("password","");
+define("database","osama_news_db");
+$conn = mysqli_connect(hostname,username,password);
+
+if(!$conn){
+    echo "خطأ في الاتصال بقاعدة البيانات: " . mysqli_connect_error();
+}
+
+
+
+//##############################################
+//Drop DataBase
+//$sql = "DROP DATABASE osama_news_db";
+
+//Create DataBase
+$sql = "CREATE DATABASE IF NOT EXISTS osama_news_db";
+
+//Implement query
+ $result_connection = mysqli_query($conn,$sql);
+
+
+ mysqli_close($conn);
+
+//##############################################
+
+$conn = mysqli_connect(hostname,username,password);
+
+if(!$conn){
+    echo "خطأ في الاتصال بقاعدة البيانات: " . mysqli_connect_error();
+}
